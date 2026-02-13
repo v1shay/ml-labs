0a51c5bec7ba597996aefb419f69844df5e3e279
v1shay <v.agrwl17@gmail.com>
2026-02-13 13:33:51 +0800

    fixing bugs cursor made

--- ml_labs/config/__init__.py
+++ ml_labs/config/__init__.py
@@ -1,13 +1,8 @@
-"""Configuration management module."""
+from .config import AppConfig
+from .logging_config import LoggingConfig, configure_logging
 
-import sys
-from pathlib import Path
-
-_parent = Path(__file__).parent.parent
-if str(_parent) not in sys.path:
-    sys.path.insert(0, str(_parent))
-
-from config import AppConfig
-from logging_config import LoggingConfig, configure_logging
-
-__all__ = ["AppConfig", "LoggingConfig", "configure_logging"]
+__all__ = [
+    "AppConfig",
+    "LoggingConfig",
+    "configure_logging",
+]
